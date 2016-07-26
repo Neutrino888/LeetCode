6cf4abe958f88a6e481477372cc4918bbb95af40
Neutrino888 <liz@smu.edu>
2016-07-25 23:31:44 -0500

    Create MedianofTwoSortedArrays.cpp

---
+++ MedianofTwoSortedArrays.cpp
@@ -0,0 +1,19 @@
+// For the first time, my solution was accepted with the first trial
+// Need to learn about std::sort, insert method for vector
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        //concatenating two arrays, and sort it
+        int N=nums1.size()+nums2.size();
+        vector<int> mvec(nums1.begin(),nums1.end());
+        mvec.insert(mvec.end(),nums2.begin(),nums2.end());
+        sort(mvec.begin(),mvec.end());
+        //return the median according to even and odd situation
+        if(N%2==1){
+            return mvec.at(N/2);
+        }else{
+            return 0.5*(mvec.at(N/2-1)+mvec.at(N/2));
+        }
+    }
+};
