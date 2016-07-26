a77079fc72b39d3dd021136d5837e1e99d4351cc
Neutrino888 <liz@smu.edu>
2016-07-25 20:43:20 -0500

    Update LongestSubstringWithoutRepeatingCharacters.cpp

--- LongestSubstringWithoutRepeatingCharacters.cpp
+++ LongestSubstringWithoutRepeatingCharacters.cpp
@@ -1,26 +1,29 @@
+/*this is really really bad, I failed too many times!!*/
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         std::unordered_map<char,int> umap;
-        int max_counter=0, temp_begin=0, temp=0;
+        int max_length=0, left=0, right=0;
 
         for(int i=0; i<s.size(); i++){
             auto search = umap.find(s[i]);
             // not found
             if(search==umap.end()){
                 umap[s[i]]=i;
-                temp++;
-            }else{
+                right++;
+            }else if(search->second>=left){
             // found
-                temp=i-temp_begin;
-                temp_begin=search->second;
-                if(temp>max_counter) max_counter=temp;
-                umap.erase(search);
+                if(right-left>max_length) max_length=right-left;
+                left=search->second+1;
+                right++;
                 umap[s[i]]=i;
+            }else if(search->second<left){
+                umap[s[i]]=i;
+                right++;
             }
         }
-        if(temp>max_counter) max_counter=temp;
+        if(right-left>max_length) max_length=right-left;
 
-        return max_counter;
+        return max_length;
     }
 };
